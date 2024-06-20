b0466a50fde55c431b39999a58b6796cbc8eee00
Bill Wang <billhhh@users.noreply.github.com>
2024-06-20 16:21:12 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -9,4 +9,4 @@ CUDA_VISIBLE_DEVICES=$1 python eval.py \
 --data_list=BraTS18/BraTS18_test.csv \
 --weight_std=True \
 --restore_from=snapshots/Eval_BraTS18_SS_[80,160,160]_SGD_b1_lr-2_alpha.1_beta.02_L1/final.pth \
---mode=1,2,3,4 > logs/${time}_train_${name}.log 2>&1 &
+--mode=0,1,2,3 > logs/${time}_train_${name}.log 2>&1 &
